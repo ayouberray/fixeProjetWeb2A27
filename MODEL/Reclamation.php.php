9ecf203d052d03b5d917b367a954710bb656e16c
Dhiasfar <Dhiaeddine.Sfar@esprit.tn>
2026-04-13 22:42:57 +0100

    Update Reclamation.php.php

--- MODEL/Reclamation.php.php
+++ MODEL/Reclamation.php.php
@@ -1,5 +1,5 @@
 <?php
-class Reclamation {
+class Reclamation{
     private ?int $id_reclamation;
     private string $reference;
     private int $id_citoyen;
@@ -14,8 +14,39 @@ class Reclamation {
     private string $date_soumission;
     private ?string $date_modification;
 
-    public function __construct($reference, $id_citoyen, $categorie, $objet, $description, $priorite = 'normale', $id_service = null, $lieu = null, $piece_jointe = null) {
-        $this->reference = $reference;
+    // Setters
+    public function setIdReclamation($id_reclamation){ $this->id_reclamation = $id_reclamation; }
+    public function setReference($reference){ $this->reference = $reference; }
+    public function setIdCitoyen($id_citoyen){ $this->id_citoyen = $id_citoyen; }
+    public function setIdService($id_service){ $this->id_service = $id_service; }
+    public function setCategorie($categorie){ $this->categorie = $categorie; }
+    public function setObjet($objet){ $this->objet = $objet; }
+    public function setDescription($description){ $this->description = $description; }
+    public function setLieu($lieu){ $this->lieu = $lieu; }
+    public function setPriorite($priorite){ $this->priorite = $priorite; }
+    public function setStatut($statut){ $this->statut = $statut; }
+    public function setPieceJointe($piece_jointe){ $this->piece_jointe = $piece_jointe; }
+    public function setDateSoumission($date_soumission){ $this->date_soumission = $date_soumission; }
+    public function setDateModification($date_modification){ $this->date_modification = $date_modification; }
+
+    // Getters
+    public function getIdReclamation(){ return $this->id_reclamation; }
+    public function getReference(){ return $this->reference; }
+    public function getIdCitoyen(){ return $this->id_citoyen; }
+    public function getIdService(){ return $this->id_service; }
+    public function getCategorie(){ return $this->categorie; }
+    public function getObjet(){ return $this->objet; }
+    public function getDescription(){ return $this->description; }
+    public function getLieu(){ return $this->lieu; }
+    public function getPriorite(){ return $this->priorite; }
+    public function getStatut(){ return $this->statut; }
+    public function getPieceJointe(){ return $this->piece_jointe; }
+    public function getDateSoumission(){ return $this->date_soumission; }
+    public function getDateModification(){ return $this->date_modification; }
+
+    // Constructeur
+    public function __construct($id_citoyen, $categorie, $objet, $description, $priorite = 'normale', $id_service = null, $lieu = null){
+        $this->reference = 'REC-' . date('Ymd') . '-' . rand(1000, 9999);
         $this->id_citoyen = $id_citoyen;
         $this->id_service = $id_service;
         $this->categorie = $categorie;
@@ -24,34 +55,9 @@ class Reclamation {
         $this->lieu = $lieu;
         $this->priorite = $priorite;
         $this->statut = 'soumise';
-        $this->piece_jointe = $piece_jointe;
+        $this->piece_jointe = null;
         $this->date_soumission = date('Y-m-d H:i:s');
+        $this->date_modification = null;
     }
-
-    // Getters
-    public function getIdReclamation() { return $this->id_reclamation; }
-    public function getReference() { return $this->reference; }
-    public function getIdCitoyen() { return $this->id_citoyen; }
-    public function getIdService() { return $this->id_service; }
-    public function getCategorie() { return $this->categorie; }
-    public function getObjet() { return $this->objet; }
-    public function getDescription() { return $this->description; }
-    public function getLieu() { return $this->lieu; }
-    public function getPriorite() { return $this->priorite; }
-    public function getStatut() { return $this->statut; }
-    public function getPieceJointe() { return $this->piece_jointe; }
-    public function getDateSoumission() { return $this->date_soumission; }
-    public function getDateModification() { return $this->date_modification; }
-    
-    // Setters
-    public function setIdReclamation($id) { $this->id_reclamation = $id; }
-    public function setStatut($statut) { $this->statut = $statut; }
-    public function setDateModification($date) { $this->date_modification = $date; }
-    public function setObjet($objet) { $this->objet = $objet; }
-    public function setDescription($description) { $this->description = $description; }
-    public function setPriorite($priorite) { $this->priorite = $priorite; }
-    public function setCategorie($categorie) { $this->categorie = $categorie; }
-    public function setLieu($lieu) { $this->lieu = $lieu; }
-    public function setIdService($id_service) { $this->id_service = $id_service; }
 }
 ?>
